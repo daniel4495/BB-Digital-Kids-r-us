763648b4eba1d653c933ea1c1d6cfbdbc11b91c9
Daniel Petti <dani4495@hotmail.com>
2022-11-12 15:08:06 -0300

    Day 2
    agregue el nav

---
+++ src/components/assets/data/data.js
@@ -0,0 +1,28 @@
+export const navlist = [
+
+    {
+        text: "home",
+        path: "/",
+    },
+    {
+        text: "shop features",
+        path: "/shop",
+    },
+    {
+        text: "clothes",
+        path: "/clothes",
+    },
+    {
+        text: "pages",
+        path: "/pages",
+    },
+    {
+        text: "shortcodes",
+        path: "/shortcodes",
+    },
+    {
+        text: "post types",
+        path: "/post",
+    },
+
+]
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import logo from "../assets/recursos/main/logo.png"
+import { navlist } from '../assets/data/data'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
@@ -12,6 +14,15 @@ export const Header = () => {
                     </div>
                     <div className="left">
                         <img src={logo} alt="" />
+                    </div>
+                    <div className="center">
+                        <ul className="menu">
+                            {navlist.map((nav) =>(
+                                <li key={nav.id}>
+                                    <Link to={nav.path}>{nav.text}</Link>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </nav>
             </div>
